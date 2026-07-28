f4f500c05b087566f88082eb4c47f0cb25eb980c
peludito5081-ux <juliancaro5081@gmail.com>
2026-07-28 11:47:02 -0500

    pachinko y introduccion a la programacion

---
+++ intentoDemuerte/Assets/_main/scripts/player.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("upDate");
+    }
+}
