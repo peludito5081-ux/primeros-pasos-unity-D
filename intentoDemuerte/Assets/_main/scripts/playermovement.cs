7fdcf5370c34bba773abc8d98656d28a28c59cb4
peludito5081-ux <juliancaro5081@gmail.com>
2026-08-11 11:57:44 -0500

    grouncheck

--- intentoDemuerte/Assets/_main/scripts/playermovement.cs
+++ intentoDemuerte/Assets/_main/scripts/playermovement.cs
@@ -13,32 +13,28 @@ public class playermovement : MonoBehaviour
     [SerializeField] private float _speed = 1.85f;
 
    [SerializeField] private Rigidbody2D _rigidbody2D;
-   
 
-
-
-    private void Start()
+    [SerializeField] private GroundCheck _groundCheck;
+    private void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
 
-
-
-
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.Space))
+        if (_groundCheck.isGround)
+        if(Input.GetKey(KeyCode.Space))
         {
             _rigidbody2D.AddForce(Vector2.up * _force);
-            
+            Debug.Log("oprimi la tecla");
         }
 
-        _rigidbody2D.velocity = Vector2.right * _speed * Time.deltaTime;
-
+        _rigidbody2D.velocity = Vector2.right * _speed;
 
 
     }
+
        
 
 }
