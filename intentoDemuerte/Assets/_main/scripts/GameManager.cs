76bbb807241aaf46a5ec73071279c2637b204b15
GaboG475 <gguerrero475@soyudemedellin.edu.co>
2026-09-17 11:45:07 -0500

    Menú

---
+++ intentoDemuerte/Assets/_main/scripts/GameManager.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public void CargarEscena(int scene)
+    {
+        SceneManager.LoadScene(scene);
+
+    }
+}
